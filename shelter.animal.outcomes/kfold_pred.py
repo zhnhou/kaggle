852c2097666d45fc76b957f1cb7128051e079ac1
zhenhou <zhn.hou@gmail.com>
2016-07-27 23:12:15 -0500

    kfolding

--- shelter.animal.outcomes/kfold_pred.py
+++ shelter.animal.outcomes/kfold_pred.py
@@ -4,9 +4,11 @@ import xgboost
 
 class kfold_xgb_pred(object):
 
-    def __init__(self, train, test, n_fold=5, n_estimators=None, max_depth=None, min_child_weight=None, gamma=None, 
-                 subsample=None, colsample_bytree=None, reg_alpha=None, reg_lambda=None, learning_rate=None, seed=None):
+    def __init__(self, train, target, test, n_fold=5, n_estimators=None, max_depth=None, min_child_weight=None, gamma=None, 
+                 subsample=None, colsample_bytree=None, reg_alpha=None, reg_lambda=None, learning_rate=None, 
+                 seed=None, missing=None):
         self.train = train
+        self.target = target
         self.test  = test
 
         self.n_fold = n_fold
@@ -21,6 +23,28 @@ class kfold_xgb_pred(object):
         self.learning_rate = learning_rate
 
         self.seed = seed
+        self.missing = missing
 
-    def xgb_train(self, train_set, test_set):
+        self.xgboost = xgboost.XGBClassifier(missing=self.missing, objective='multi:softprob', seed=self.seed, 
+        max_depth=self.max_depth, min_child_weight=self.min_child_weight, gamma=self.gamma, subsample=self.subsample, 
+        colsample_bytree=self.colsample_bytree, reg_alpha=self.reg_alpha, reg_lambda=self.reg_lambda,
+        learning_rate=self.learning_rate)
+
+    def kfold_train_predict(self):
+
+        n_examples = self.train.shape[0]
+
+        kf = KFold(n_examples, n_folds=self.n_fold, shuffle=False)
         
+        for train_index, test_index in kf:
+            X_train = self.train.loc[train_idx]
+            y_train = self.target.loc[train_idx]
+
+            X_holdout = self.train.loc[test_idx]
+
+            xgb_train = self.xgboost.fit(X_train, y_train)
+            y_predict = xgb_train.predict_proba(X_holdout)
+            test_predict = xgb.train.predict_proba(self.test)
+
+
+
