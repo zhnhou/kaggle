2b2037da229f5b5c6a6743d59608e88d0e040796
Zhen Hou <zhn.hou@gmail.com>
2016-08-22 11:28:01 -0700

    better understanding now

--- BoschProductionLinePerformance/preprocess.py
+++ BoschProductionLinePerformance/preprocess.py
@@ -2,6 +2,10 @@ import os
 import pandas as pd
 import numpy as np
 
+'''
+We need to check the maximum/minimum value of the negative/positive categories!
+'''
+
 class BPLP_PreProcess(object):
     def __init__(self):
         self.name = 'BoschProductionLinePerformance'
@@ -17,6 +21,7 @@ class BPLP_PreProcess(object):
         '''
         We remove "T" from the csv file, using int number to identify categories.
         Just a simple sed command to do this job
+        I have checked that there is no "T" in column names
         '''
 
         self.train_categorical_file = self.data_path + 'train_categorical_int.csv'
@@ -26,22 +31,28 @@ class BPLP_PreProcess(object):
 
         nrows_skip = 0
         nrows_read = nrows_bundle
+        df_cat = pd.DataFrame([])
         while nrows_read == nrows_bundle:
             # use skiprows = range(...)
-            tmp = pd.read_csv(self.train_categorical_file, nrows=nrows_bundle, skiprows=np.arange(nrows_skip-1)+1)
+            skiprows = np.arange(nrows_skip)+1
+            tmp = pd.read_csv(self.train_categorical_file, nrows=nrows_bundle, skiprows=skiprows, dtype=np.float32)
             nrows_read = tmp.shape[0]
             nrows_skip += nrows_read
-            print "Read "+str(nrows_read)+' lines'
+
+            if nrows_skip == nrows_read:
+                line_start = 1
+            else:
+                line_start = skiprows[-1]+1
+
+            line_end = line_start + nrows_read - 1
+            print "Read "+str(nrows_read)+" lines, from "+str(line_start)+" to "+str(line_end)
 
             tmp[tmp < -2147480000] += 2147480000
             tmp = tmp.fillna( self.missval )
 
             features = tmp.columns.delete(0) # here remove 'Id' column
 
-            if nrows_skip == nrows_read:
-                df_cat = tmp[features].astype(np.int16)
-            else:
-                df_cat.append(tmp[features].astype(np.int16), ignore_index=True)
+            df_cat = df_cat.append(tmp[features].astype(np.int16), ignore_index=True)
 
         return df_cat
 
